9d0dc90bf07fd1f576a46b911f02c0279f5cc961
Yeahhooo <3266325505@qq.com>
2023-09-29 00:03:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>ads</div>
+</template>
+
+<script setup lang="ts">
+const test = () => {
+  const remp = 'asd'
+}
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 10px;
+}
+</style>
